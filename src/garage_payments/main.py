e2a496380e3a87087dcd725a6663b62d1d15604f
AlexBorealis <kozlovaleksandr592@gmail.com>
2025-07-31 12:47:16 +0700

    feature: добавил функцию для создания даты следующего платежа next_payment_datetime

--- src/garage_payments/main.py
+++ src/garage_payments/main.py
@@ -3,10 +3,12 @@ from garages.garages import garages
 from utils.utils import next_payment_datetime
 from datetime import datetime
 
+import pandas as pd
+
 if __name__ == "__main__":
-    #current_datetime = datetime.strptime("01.03.2025", "%d.%m.%Y")
+    # current_datetime = datetime.strptime("11.03.2025", "%d.%m.%Y")
     current_datetime = datetime.now()
     joined_table = garages.merge(payments, how="left", on="amount")
-    # joined_table["status"] = joined_table.apply(next_payment_datetime, current_datetime=current_datetime, axis=1)
+    joined_table["payment_datetime"] = next_payment_datetime(joined_table, current_datetime)
 
     print(joined_table)
--- src/garage_payments/utils/utils.py
+++ src/garage_payments/utils/utils.py
@@ -1,11 +1,11 @@
-from datetime import datetime, timedelta
-from pytz import timezone
+from datetime import datetime, timezone
 from dateutil.relativedelta import relativedelta
+from pytz import timezone
 
-import pandas as pd
+from pandas import Series, to_datetime, DataFrame
 
 
-def get_number_column(object: pd.DataFrame, pattern: str) -> int | None:
+def get_number_column(object: DataFrame, pattern: str) -> int | None:
     """
     Находит индекс первого столбца, содержащего значение, соответствующее заданному шаблону.
 
@@ -30,7 +30,9 @@ def get_number_column(object: pd.DataFrame, pattern: str) -> int | None:
         return None
 
 
-def tz(object: pd.DataFrame, old: str, new: str, format: str = "%d.%m.%Y %H:%M") -> pd.DataFrame:
+def tz(
+    object: DataFrame, old: str, new: str, format: str = "%d.%m.%Y %H:%M"
+) -> DataFrame:
     """
 
     :param object: pandas DataFrame
@@ -39,10 +41,10 @@ def tz(object: pd.DataFrame, old: str, new: str, format: str = "%d.%m.%Y %H:%M")
     :param format: Формат даты
     :return: Столбец DataFrame в формате datetime
     """
-    return pd.to_datetime(object, format=format).dt.tz_localize(old).dt.tz_convert(new)
+    return to_datetime(object, format=format).dt.tz_localize(old).dt.tz_convert(new)
 
 
-def to_float(object, replacements: list[tuple[str, str]]) -> pd.DataFrame:
+def to_float(object, replacements: list[tuple[str, str]]) -> DataFrame:
     """
 
     :param object: pandas DataFrame
@@ -55,31 +57,21 @@ def to_float(object, replacements: list[tuple[str, str]]) -> pd.DataFrame:
     return result.astype(float)
 
 
-def next_payment_datetime(object: pd.DataFrame, current_datetime: datetime, tz: str = "Asia/Novosibirsk") -> pd.DataFrame:
-    init_date = object['initial_datetime']
-    next_payment = current_datetime.astimezone(timezone(zone=tz))
+def next_payment_datetime(
+    object: DataFrame, current_datetime: datetime, zone: str = "Asia/Novosibirsk"
+) -> Series:
+    current_datetime = current_datetime.astimezone(timezone(zone=zone))
 
-    # Получаем разницу в месяцах между текущей датой оплаты и начальной датой
-    months_diff = (next_payment.year - init_date.year) * 12 + next_payment.month - init_date.month
+    month_delta = (
+        (current_datetime.year - object.initial_datetime.dt.year) * 12
+        + (current_datetime.month - object.initial_datetime.dt.month)
+        + 1
+    )
 
-    # Вычисляем следующую дату оплаты (следующий месяц)
-    next_payment += relativedelta(months=1)
-
-    # Корректировка для високосного года (29 февраля)
-    if init_date.day == 29 and init_date.month == 2:
-        if not (next_payment.year % 4 == 0 and (next_payment.year % 100 != 0 or next_payment.year % 400 == 0)):
-            next_payment = next_payment.replace(day=28)
-
-    # Добавляем 3 дня к дате оплаты
-    next_payment += timedelta(days=3)
-
-    # Проверяем, что следующая дата оплаты позже текущей даты
-    if next_payment.date() <= current_datetime.date():
-        next_payment = next_payment + relativedelta(months=1)
-        # Повторная корректировка для високосного года
-        if init_date.day == 29 and init_date.month == 2:
-            if not (next_payment.year % 4 == 0 and (next_payment.year % 100 != 0 or next_payment.year % 400 == 0)):
-                next_payment = next_payment.replace(day=28)
-        next_payment = next_payment + timedelta(days=3)
-
-    return next_payment
+    return Series(
+        [
+            d + relativedelta(months=m)
+            for d, m in zip(object.initial_datetime, month_delta)
+        ],
+        index=object.initial_datetime.index,
+    )
